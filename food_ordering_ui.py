e7c3b9aab9de0af874265a65e9499d816568c91c
Solomon Antony <santony@murraystate.edu>
2024-09-13 21:37:00 -0500

    updated5

--- food_ordering_ui.py
+++ food_ordering_ui.py
@@ -6,18 +6,25 @@ def show_main_menu():
   while True:
     print("Solomon diner") #edit to show your name
     print("__________")
-    print('Enter', 'N for a new order','|', 'X for prepare the check','|', 'Q for quit' )
+    print('Enter', 'N for a new order','|', 'E for edit order', '|', 'X for prepare the check','|', 'Q for quit' )
     user_menu_choice = input('Your choice: ')
     if user_menu_choice in 'Qq':
+      print('This option ends the application')
       break
     elif user_menu_choice in 'Xx':
       print('This option prints the list of items ordered, extended price, total, Taxes, and Grand total ')
     elif user_menu_choice in 'Nn':
-      ordered_item = functions.get_item_number()
-      current_order.append(ordered_item)
-      print(current_order)
+      print('This option adds dishes to orders')
+      while input('Add a dish? y/n: ') in 'Yy':
+        ordered_item = functions.get_item_number()
+        current_order.append(ordered_item)
+      print('Current order', current_order)
+    elif user_menu_choice in 'Ee':
+      print('This option edits the current order')
     else:
-      make_order(user_menu_choice.upper())  #calls a function for adding to the orders
+      print('Incorrect choice.  Goddbye!')
+      break
+
 
 def make_order(menu_choice):
   print('Functionality for menu choice ', menu_choice)
